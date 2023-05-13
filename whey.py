ecb7bb1bd15dbbdf205d3fe915352c1904858965
rafaeldk123 <133514782+rafaeldk123@users.noreply.github.com>
2023-05-13 11:47:30 -0300

    Update whey.py

--- whey.py
+++ whey.py
@@ -8,7 +8,7 @@ st.title('Calculadora de Whey')
 
 peso = st.number_input('Peso total do produto (g):',min_value=1,max_value=9999999999999,value=900)
 qtd = st.number_input('Quantidade por porção (g):',min_value=1,max_value=9999999999999,value=30)
-qtd_prot = st.number_input('Quantidade de proteína por porção (g):',min_value=1,max_value=qtd,value=24)
+qtd_prot = st.number_input('Quantidade de proteína por porção (g):',min_value=1,max_value=9999999999999,value=24)
 valor = st.number_input('Valor do produto:',min_value=1,max_value=9999999999999,value=99)
 
 peso=int(peso)
@@ -16,18 +16,12 @@ qtd=int(qtd)
 qtd_prot=int(qtd_prot)
 valor=int(valor)
 
-
 percprot=int(round((qtd_prot*100/qtd)))
 valor100gprot=round(100*(valor/(peso*(qtd_prot/qtd))),2)
 st.write(f'**Percentual de proteína:** {percprot}%')
+if percprot>100:
+    st.warning('Por favor, ajuste os valores.', icon=None)
 st.write(f'**Valor de 100g de proteína:** R${valor100gprot}')
-
-
-#peso=int(peso)
-#qtd=int(qtd)
-#qtd_prot=int(qtd_prot)
-#valor=int(valor)
-
 
 
 
